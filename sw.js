b7060f2f84efe95134c9133248faf97a53ee7748
Fabien <canape-silence-8o@icloud.com>
2026-09-20 22:29:29 +0200

    chore: renouvelle le cache premium

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courses-app-v7-list-overhaul';
+const CACHE='courses-app-v8-premium';
 const SHELL=['./','./index.html','./styles.css','./catalog.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
